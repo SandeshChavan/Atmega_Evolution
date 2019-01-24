a07bea17e14d3d4071ffc13411e45b7ed91832ce
SandeshChavan <46970114+SandeshChavan@users.noreply.github.com>
2019-01-24 07:27:10 +0530

    push 3

--- arduinoUno/arduinoUno.ino
+++ arduinoUno/arduinoUno.ino
@@ -2,8 +2,8 @@
 #include<dht.h>
 #include<ArduinoJson.h>
 #include<SoftwareSerial.h>
-SoftwareSerial s(5,6);
-SoftwareSerial d(5,6);//Establishing recive and transmittion ports Rx and Tx
+SoftwareSerial s(5,6);//Establishing recive and transmittion ports Rx and Tx for writing to NodeMCU
+SoftwareSerial d(5,6);//Establishing recive and transmittion ports Rx and Tx for reading from NodeMCU
 LiquidCrystal myLCD(12,11,5,4,3,2);//Establishing connection to lcd
 dht DHT;
 int dhtPin=3;//Humidity and temperature port
@@ -11,30 +11,39 @@ int mq135Pin=A0;//Co2 sensor
 int mq4Pin=A1;//Airquality sensor
 int soilMoisturePin=5;//SoilMoisture pin
 int soilMoistureValue;
-int mq4Value;
+int coolentFan=5;
+int  ledPin=12;
+void runFan();
+void runPump();
+void lcdDisplay();
 int mq135Value;
+StaticJsonBuffer<1000> jsonBuffer1;//Buffer for recieving data
+JsonObject& root1=jsonBuffer1.parseObject(d);
+
 void setup() {
+  myLCD.begin(16,2);
+  pinMode(ledPin,OUTPUT);
   pinMode(dhtPin,INPUT);
   pinMode(mq135Pin,INPUT);
   pinMode(mq4Pin,INPUT);
   pinMode(soilMoisturePin,INPUT);
   Serial.begin(9600);//Estblishing communication with the serial port
   s.begin(115200);
+  d.begin(115200);
   DHT.read11(dhtPin);
+   runFan();
+   runPump();
+   lcdDisplay();  
 }
 
 void loop() 
 {
-  mq4Value=analogRead(mq4Pin);
-  mq135Value=analogRead(mq135Pin);
+  int mq4Value=analogRead(mq4Pin);
+   mq135Value=analogRead(mq135Pin);
   soilMoistureValue=analogRead(soilMoisturePin);
+  soilMoistureValue=map(soilMoistureValue,550,0,0,100);
   StaticJsonBuffer<1000> jsonBuffer;
-  StaticJsonBuffer<1000> jsonBuffer1;
-  JsonObject& root= jsonBuffer.createObject();
-  JsonObject& root1=jsonBuffer1.parseObject(d);
-  root.prettyPrintTo(Serial); 
-  if(root1 == JsonObject::invalid())
-    return; 
+  JsonObject& root= jsonBuffer.createObject();   
   root["Humidity"] =DHT.humidity;
   root["Temperature"] =DHT.temperature;
   root["soilMoisture"]=soilMoistureValue;
@@ -43,6 +52,82 @@ void loop()
   if(s.available()>0)
   {
     root.printTo(s);
-  }  
-
+  }
+  //Communication from nodeMCU
+  if(root1 == JsonObject::invalid())
+    return;
+   root1.prettyPrintTo(Serial); 
 }
+
+void runFan()
+{
+ DHT.read11(dhtPin);
+ int dhtHum=DHT.humidity;//reads the humidity from the surrounding;
+ delay(100);
+ int dhtTemp=DHT.temperature;//reads the temperature from the surrounding;
+ delay(100);
+ if(dhtTemp>=31)
+ {
+  analogWrite(coolentFan,1024);
+ }
+ else if(dhtTemp<=30&&dhtTemp>=27)
+ {
+   switch(dhtTemp)
+   {
+    case 30:analogWrite(coolentFan,1024);
+           break;
+    case 29:analogWrite(coolentFan,682);
+           break;  
+    case 28:analogWrite(coolentFan,342);
+          break; 
+    default:analogWrite(coolentFan,0);
+          break;                 
+    }
+ }
+ else
+ {
+  digitalWrite(ledPin,HIGH);
+ }
+ if(DHT.humidity<60)
+ {
+  analogWrite(ledPin,1024);
+ }
+ if(int fanStatus=root1["fanStatus"])
+  {
+   analogWrite(coolentFan,HIGH);
+  }
+}
+
+void runPump()
+{
+  if(soilMoistureValue<30)
+  {
+   analogWrite(soilMoisturePin,HIGH);
+  }
+  if(int motorStatus=root1["motorStatus"])
+  {
+   analogWrite(soilMoisturePin,HIGH);
+  }  
+}
+  
+void lcdDisplay()
+{
+    myLCD.setCursor(0,0);
+    myLCD.print("Hum:");
+    myLCD.setCursor(5,0);
+    myLCD.print(DHT.humidity);
+    myLCD.setCursor(9,0);
+    myLCD.print("tem:");
+    myLCD.setCursor(12,0);
+    myLCD.print(DHT.temperature);
+    myLCD.setCursor(0,1);
+    myLCD.print("soM:");
+    myLCD.setCursor(5,0);
+    myLCD.print(soilMoistureValue);
+    myLCD.setCursor(9,0);
+    myLCD.print("Co2:");
+    myLCD.setCursor(12,0);
+    myLCD.print(mq135Value);
+    
+    }
+  
--- nodeMCU/nodeMCU.ino
+++ nodeMCU/nodeMCU.ino
@@ -10,6 +10,7 @@ ESP8266WiFiMulti wifiMulti;
 ESP8266WebServer server(80);//Server port for communication
 SoftwareSerial s(D6,D5);//Establishing recive and transmittion ports Rx and Tx
 SoftwareSerial d(D5,D6);
+int fanStatus=0;
 int motorStatus=0;
 int dhtHumidityValue;
 int dhtTemperatureValue;
@@ -17,45 +18,58 @@ int soilMoistureValue;
 int mq4Value;
 int mq135Value;
 int led=D5;
+
 const char* ssid="AtmegaEvolution";
 const char* password="12345678";
-String getContentType(String filename);
 void handleFileRead();
 void handleNotFound();
 void onMotor();
-void runFan();
-void runPump();
+void onFan();
+void handleHum();
+void handleTemp();
+void handleSoil();
+void handleAir();
+void handleCO2();
+void handleGraph();
 StaticJsonBuffer<1000> jsonBuffer1;
 JsonObject& root1=jsonBuffer1.createObject();
-  
+
 void setup()
 {
-  pinMode(led,OUTPUT);
- Serial.begin(115200);
- s.begin(115200);//Baut rate for communication of nodeMCU and arduinoUNO
- while(!Serial)continue;
- Serial.print("Connecting to ssid please wait");
- wifiMulti.addAP("Sandy","12345678");
- wifiMulti.addAP("MSI","12345678");
- while(wifiMulti.run()!=WL_CONNECTED)
- {
-  delay(1000);
-  Serial.print(".");
- }
- if(MDNS.begin("AtmegaEvolution"))
+   pinMode(led,OUTPUT);
+  Serial.begin(115200);
+  d.begin(115200);
+  s.begin(115200);//Baut rate for communication of nodeMCU and arduinoUNO
+  while(!Serial)continue;
+  Serial.print("Connecting to ssid please wait");
+  wifiMulti.addAP("Sandy","12345678");
+  wifiMulti.addAP("MSI","12345678");
+  while(wifiMulti.run()!=WL_CONNECTED)
   {
-    Serial.println("The mDNS is ready for use"); 
+   delay(1000);
+   Serial.print(".");
   }
- else
-  {
+  if(MDNS.begin("AtmegaEvolution"))
+   {
+     Serial.println("The mDNS is ready for use"); 
+   } 
+  else
+   {
     Serial.println("The mDNS is not ready for use"); 
-  }
-  SPIFFS.begin();
-  server.onNotFound(handleNotFound);
-  server.on("/",HTTP_GET,handleFileRead); // Call the 'handleFileRoot' function when a client requests URI "/"
-  server.on("/LED", HTTP_POST,onMotor);
-  server.begin();
-  Serial.print(WiFi.localIP());
+   } 
+   SPIFFS.begin();
+   server.onNotFound(handleNotFound);
+   server.on("/",HTTP_GET,handleFileRead); // Call the 'handleFileRoot' function when a client requests URI "/"
+   server.on("/onMotor", HTTP_POST,onMotor);
+   server.on("/onFan", HTTP_POST,onFan);
+   server.on("/getHum", HTTP_POST,handleHum);
+   server.on("/getTemp", HTTP_POST,handleTemp);
+   server.on("/getSoil", HTTP_POST,handleSoil);
+   server.on("/getCO2", HTTP_POST,handleCO2);
+   server.on("/getAir", HTTP_POST,handleAir);
+   server.on("/plotGraph", HTTP_POST,handleGraph);
+   server.begin();
+   Serial.print(WiFi.localIP());
 }
 
 void loop()
@@ -66,25 +80,20 @@ void loop()
   if(root == JsonObject::invalid())
     return; 
     root.prettyPrintTo(Serial); 
- if(d.available()>0)
-  {
-    root1.printTo(d);
-  }  
   dhtHumidityValue=root["Humidity"];
   dhtTemperatureValue=root["Temperature"];
   soilMoistureValue=root["soilMoisture"];
   mq4Value=root["mq4"];
-  mq135Value=root["mq135"];  
+  mq135Value=root["mq135"];
+ //Writing to arduino
+  if(d.available()>0)
+  {
+    root1.printTo(d);
+  }  
+   
   delay(100000); 
 }
 
-String getContentType(String filename) { // convert the file extension to the MIME type
-if (filename.endsWith(".html")) return "text/html";
-else if (filename.endsWith(".css")) return "text/css";
-else if (filename.endsWith(".js")) return "application/javascript";
-else if (filename.endsWith(".ico")) return "image/x-icon";
-return "text/plain";
-}
 
 void handleFileRead()
 {
@@ -94,14 +103,8 @@ void handleFileRead()
  size_t sent = server.streamFile(file, "text/html"); // And send it to the client
  file.close(); // Then close the file again
 }
-Serial.println("\tFile Not Found");
+Serial.println("\File Not Found");
 }
-
-void runFan()
-{}
-void runPump()
-{}
-
 
 void onMotor()
 {
@@ -110,8 +113,42 @@ void onMotor()
 }
 
 
+void onFan()
+{
+   root1["fanStatus"]=!fanStatus;
+   handleFileRead();
+}
 
+void handleHum()
+{
+ server.send(200, "text/plane", String(dhtHumidityValue)); 
+  }
+void handleTemp()
+{
+  server.send(200, "text/plane", String(dhtTemperatureValue));
+}
+void handleSoil()
+{
+  server.send(200, "text/plane", String(soilMoistureValue));
+}
+void handleAir()
+{
+  server.send(200, "text/plane", String(mq4Value));
+}
+void handleCO2()
+{
+  server.send(200, "text/plane", String(mq135Value));
+}
 
+void handleGraph()
+{
+  String path="/graph.html";
+  if (SPIFFS.exists(path)) { // If the file exists
+ File file = SPIFFS.open(path, "r"); // Open it
+ size_t sent = server.streamFile(file, "text/html"); // And send it to the client
+ file.close(); // Then close the file again
+
+}
 void handleNotFound(){
 server.send(404, "text/plain", "404: Not found");
 }
